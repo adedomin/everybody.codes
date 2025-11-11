90f0430e4daf08318273037b38d5095ec78d5176
Anthony DeDominic <adedomin@gmail.com>
2025-11-11 11:02:58 -0500

    reduce bloat

--- y2025/src/bin/2025-q06.rs
+++ y2025/src/bin/2025-q06.rs
@@ -16,22 +16,7 @@ pub fn read_input_as_str() -> io::Result<String> {
 
 const ALPHA_START: u8 = b'A';
 const ALPHABET_SZ: usize = 26;
-
-fn parse(list: &[u8]) -> [usize; ALPHABET_SZ] {
-    let mut hs = [0; ALPHABET_SZ];
-    list.iter()
-        .enumerate()
-        .flat_map(|(idx, knight)| knight.is_ascii_uppercase().then_some((idx, *knight)))
-        .for_each(|(idx, knight)| {
-            let entry = &mut hs[(knight - ALPHA_START) as usize];
-            *entry += list[idx..]
-                .iter()
-                .filter(|&&k| k.is_ascii_lowercase() && k.to_ascii_uppercase() == knight)
-                .count();
-        });
-    hs
-}
-
+const SWORD_KNIGHT_IDX: usize = 0;
 const LIM: usize = 1000;
 const REPEAT: usize = 1000;
 
@@ -41,32 +26,32 @@ fn p3_range(idx: usize, len: usize) -> std::ops::Range<usize> {
     start..end
 }
 
-fn p3(list: &[u8]) -> usize {
+fn parse(list: &[u8], p3: bool) -> [usize; ALPHABET_SZ] {
     let mut hs = [0; ALPHABET_SZ];
+    let len = list.len();
     list.iter()
         .enumerate()
-        .flat_map(|(idx, novice)| novice.is_ascii_lowercase().then_some((idx, *novice)))
-        .for_each(|(idx, novice)| {
-            let entry = &mut hs[(novice - b'a') as usize];
-            *entry += list[p3_range(idx, list.len())]
+        .flat_map(|(idx, knight)| knight.is_ascii_uppercase().then_some((idx, *knight)))
+        .for_each(|(idx, knight)| {
+            let entry = &mut hs[(knight - ALPHA_START) as usize];
+            let range = if p3 { p3_range(idx, len) } else { idx..len };
+            *entry += list[range]
                 .iter()
-                .filter(|&&k| k.is_ascii_uppercase() && k.to_ascii_lowercase() == novice)
+                .filter(|&&k| k.is_ascii_lowercase() && k.to_ascii_uppercase() == knight)
                 .count();
         });
-    hs.iter().sum::<usize>()
+    hs
 }
-
-const SWORD_KNIGHT_IDX: usize = 0;
 
 fn main() -> io::Result<()> {
     let inp = read_input_as_str()?;
     let inp = inp.trim();
-    let listof = parse(inp.as_bytes());
+    let listof = parse(inp.as_bytes(), false);
     let part1 = listof[SWORD_KNIGHT_IDX];
     let part2 = listof.iter().sum::<usize>();
 
     let inp = inp.repeat(REPEAT);
-    let part3 = p3(inp.as_bytes());
+    let part3 = parse(inp.as_bytes(), true).iter().sum::<usize>();
     println!("Part1: {part1}, Part2: {part2}, Part3: {part3}");
     Ok(())
 }
